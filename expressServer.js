1765aae8d85b4c091f9b73ce6e48bd926c3d699b
riyaverma <techriya054@gmail.com>
2026-04-13 15:11:50 +0530

    routing and query parameters in the url

--- expressServer.js
+++ expressServer.js
@@ -1,9 +1,21 @@
 const express = require('express');
 
 const app = express();
+//this .use let all kind of request to be handled by the callback function, it can be get, post,put, delete,patch, optons, head, trace, connect, all    
 
-app.use('/test', (res, req)=>{
+app.use('/test', (req, res)=>{
+    //this will handle all the request which is coming to /test route
+    const queryParams = req.query; //this will give us the query parameters in the url, it will be an object
+
+    console.log(queryParams)
     res.send("namaste developers")
+})
+app.use('/hello',(req, res) =>{
+    res.send("hello world");
+})
+
+app.use('/',(req, res) =>{
+    res.send("welcome to express server");
 })
 
 app.listen(3000, ()=>{
